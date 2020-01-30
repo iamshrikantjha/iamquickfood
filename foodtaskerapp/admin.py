04c517c1fc9be8567110334ac91a818bf5840c86
root <root@localhost.localdomain>
2020-01-30 23:05:12 +0530

    register login logout

---
+++ foodtaskerapp/admin.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+
+# Register your models here.
+from foodtaskerapp.models import Restaurant
+
+admin.site.register(Restaurant)
